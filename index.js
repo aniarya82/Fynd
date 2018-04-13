97248e6819bad6176b2e06a1441a63b12421fc70
aniarya82 <hiten.iitkgp@gmail.com>
2018-04-13 21:47:39 +0530

    added comments

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+var config = require('./config.js')
+var readData = require('./utils/readData.js')
+var dataCleanUp = require('./utils/dataCleanUp.js')
 var getTopThree = require('./utils/getTopThree.js')
 var sortPopularity = require('./utils/sortPopularity.js')
-var dataCleanUp = require('./utils/dataCleanUp.js')
-var readData = require('./utils/readData.js')
 
-var data = readData('data/imdb.json')
+// Reads the data from JSON file
+var data = readData('data/'+config.fileName)
+
+// Data is cleaned to form Hash Maps with genre
 var cleaned = dataCleanUp(data)
-var action_list = cleaned["Action"]
+
+// Retrieve the list of movies of specific genre
+var action_list = cleaned[config.genre]
+
+// Sort the list according to the popularity
 var desc_order = sortPopularity(action_list, "desc")
+
+// Finally logs the top three movies requested
 console.log(getTopThree(desc_order))
