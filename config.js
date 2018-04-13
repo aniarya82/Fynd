8ca46bb5ce2806810706b0d1e81a713332512ce2
aniarya82 <hiten.iitkgp@gmail.com>
2018-04-13 21:47:19 +0530

    added config file

---
+++ config.js
@@ -0,0 +1,8 @@
+// Give the file name of the json file
+var fileName = "imdb.json"
+
+// Give the genre required
+var genreRequested = "Comedy"
+
+module.exports.fileName = fileName
+module.exports.genre = genreRequested
